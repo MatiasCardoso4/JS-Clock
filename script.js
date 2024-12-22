3607b009fc700fb68d390e896ab8f564bd86b79b
MatiasCardoso4 <cardosomatias1994@gmail.com>
2024-12-21 21:37:13 -0300

    JS-Clock

---
+++ script.js
@@ -0,0 +1,20 @@
+const secondHand = document.querySelector(".secondHand");
+const minuteHand = document.querySelector(".minuteHand");
+const hourHand = document.querySelector(".hourHand");
+
+const setClock = () => {
+  const currentTime = new Date();
+  const seconds = currentTime.getSeconds();
+  const secondDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  const minutes = currentTime.getMinutes();
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hour = currentTime.getHours();
+  const hourDegrees = (hour / 24) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+};
+
+setInterval(setClock, 1000);
